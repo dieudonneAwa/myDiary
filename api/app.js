9c3af7649f2b47a38e9925a8320f96bbe43c30e6
awadieudone <dieudonneawa7@gmail.com>
2020-01-03 16:49:41 +0100

    setup express server

--- api/app.js
+++ api/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import logger from 'morgan';
 import cors from 'cors';
+import routes from './routes';
 
 const app = express();
 
@@ -8,5 +10,8 @@ app.use(cors({ credentials: true, origin: true }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(logger(':method :url :status :res[content-length] - :response-time ms'));
+
+routes(app);
 
 export default app;
