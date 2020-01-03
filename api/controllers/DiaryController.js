4d01e70326cc30a4d91dbe998f7a6525753d545d
awadieudone <dieudonneawa7@gmail.com>
2020-01-03 23:47:53 +0100

    create diary controller and repository

---
+++ api/controllers/DiaryController.js
@@ -0,0 +1,15 @@
+import { DiaryRepo } from "../repositories";
+
+class DiaryController {
+  static async creatDiary({ body, user }, res, next) {
+    try {
+      const { name, description } = body;
+      const diary = await DiaryRepo.create({ name, description, userId: user.id });
+      return res.status(201).send({ status: 'success', data: diary.dataValues });
+    } catch (e) {
+      return next(new Error(e));
+    }
+  }
+}
+
+export default DiaryController;
