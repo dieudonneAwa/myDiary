b13cee926d7fa0486dd41f86b046c8844b78260c
awadieudone <dieudonneawa7@gmail.com>
2020-01-04 14:20:39 +0100

    fix problem with gitignore

--- api/database/config/config.js
+++ api/database/config/config.js
@@ -13,7 +13,7 @@ module.exports = {
     logging: false,
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: 'DATABASE_URL_PROD',
     dialect: 'postgres',
   },
 };
